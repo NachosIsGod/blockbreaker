803548e3ee48d801cfb2eff984719f831a1f11fa
岩崎雄大 <yudai2019@outlook.jp>
2021-10-16 15:00:54 +0900

    ブロック表示するかの変数名をisVisiblに変更

--- src/yudai/processing/Block.java
+++ src/yudai/processing/Block.java
@@ -5,17 +5,17 @@ public class Block {
     int by;
     int bw = 25;
     int bh = 10;
-    boolean isShow = true;
+    boolean isVisibl = true;
 
     public void draw(Main main) {
         main.strokeWeight(1);
-        if(isShow)main.rect(bx, by, bw, bh);
+        if(isVisibl)main.rect(bx, by, bw, bh);
     }
 
     boolean isHit(int x, int y, int s){
-        if(isShow) {
-            isShow = !(y + s / 2 > by && y - s / 2 < by + bh && x + s / 2 > bx && x - s / 2 < bx + bw);//当たってたらfalseいなかったらtrue
-            return isShow;
+        if(isVisibl) {
+            isVisibl = !(y + s / 2 > by && y - s / 2 < by + bh && x + s / 2 > bx && x - s / 2 < bx + bw);//当たってたらfalseいなかったらtrue
+            return isVisibl;
         }
         return true;
     }
